28b88356da7d304602794cd4c95334403b93dc2f
Mauyr Alexandre Pereira <mauyr.pereira@inovapro.com.br>
2016-12-10 10:51:25 -0200

    5. Pesquisas orientado a objetos com JPQL

---
+++ src/main/java/br/com/caelum/teste/TesteConsulta.java
@@ -0,0 +1,40 @@
+package br.com.caelum.teste;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by MauyrAlexandre on 10/12/2016.
+ */
+public class TesteConsulta {
+
+    public static void main(String[] args) {
+
+        EntityManager manager = new JPAUtil().getEntityManager();
+
+        Conta conta = new Conta();
+        conta.setId(1);
+
+        Query query = manager
+                .createQuery("select m from Movimentacao m where m.conta=:pConta and m.tipoMovimentacao=:pTipo");
+
+        query.setParameter("pConta", conta);
+        query.setParameter("pTipo", TipoMovimentacao.SAIDA);
+
+        List<Movimentacao> movimentacoes = query.getResultList();
+
+        for (Movimentacao m : movimentacoes) {
+            System.out.println("\nDescricao ..: " + m.getDescricao());
+            System.out.println("Valor ......: R$ " + m.getValor());
+        }
+
+        manager.close();
+
+    }
+}
