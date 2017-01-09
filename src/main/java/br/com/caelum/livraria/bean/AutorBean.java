14021bce407c7dd4f717eb8cde038ef3bd74784f
mauyr <pqwtal49>
2017-01-09 12:04:49 -0200

    Estrutura para curso com entidades da livraria

---
+++ src/main/java/br/com/caelum/livraria/bean/AutorBean.java
@@ -0,0 +1,22 @@
+package br.com.caelum.livraria.bean;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.model.Autor;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class AutorBean {
+
+	private Autor autor = new Autor();
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando autor " + this.autor.getNome());
+
+		new DAO<Autor>(Autor.class).adiciona(this.autor);
+	}
+}
---
+++ src/main/java/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,27 @@
+package br.com.caelum.livraria.bean;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.model.Livro;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	}
+
+}
