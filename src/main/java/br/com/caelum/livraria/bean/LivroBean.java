c582cd69ac4217a731ed3804b84fcc019088c962
mauyr <pqwtal49>
2017-01-16 14:42:05 -0200

    4. Completando o sistema e lidando com escopos do JSF 2

--- src/main/java/br/com/caelum/livraria/bean/LivroBean.java
+++ src/main/java/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,14 +1,29 @@
 package br.com.caelum.livraria.bean;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.model.Autor;
 import br.com.caelum.livraria.model.Livro;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+
+	private Integer autorId;
+
+	public List<Autor> getAutores() {
+		return new DAO<>(Autor.class).listaTodos();
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
 
 	public Livro getLivro() {
 		return livro;
@@ -21,7 +36,24 @@ public class LivroBean {
 			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
 		}
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		new DAO<>(Livro.class).adiciona(this.livro);
+		this.livro = new Livro();
 	}
 
+	public void gravarAutor() {
+		Autor autor = new DAO<>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+
+	public List<Livro> getLivros() {
+		return new DAO<>(Livro.class).listaTodos();
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
 }
