1d701870e3376478aac91f6700f1d37ce42b3ce7
mauyr <pqwtal49>
2017-01-17 16:09:09 -0200

    JSF: 13. Logout e o escopo Flash

--- src/main/java/br/com/caelum/livraria/bean/LoginBean.java
+++ src/main/java/br/com/caelum/livraria/bean/LoginBean.java
@@ -6,6 +6,7 @@ import br.com.caelum.livraria.model.Autor;
 import br.com.caelum.livraria.model.Usuario;
 import br.com.caelum.livraria.util.RedirectView;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -25,16 +26,28 @@ public class LoginBean {
         System.out.println("Fazendo login do usuário "
                 + this.usuario.getEmail());
 
+        FacesContext context = FacesContext.getCurrentInstance();
         boolean existe = new UsuarioDao().existe(this.usuario);
 
         if (existe) {
-            FacesContext context = FacesContext.getCurrentInstance();
+
             context.getExternalContext().getSessionMap()
                     .put("usuarioLogado", this.usuario);
 
             return "livro?faces-redirect=true";
         }
 
-        return null;
+        context.getExternalContext().getFlash().setKeepMessages(true);
+        context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+
+        return "login?faces-redirect=true";
+    }
+
+    public String deslogar() {
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().remove("usuarioLogado");
+
+        return "login?faces-redirect=true";
     }
 }
